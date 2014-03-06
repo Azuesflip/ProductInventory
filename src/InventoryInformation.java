e09f8585a6595c4dee32339d36f78068e1e8655d
Azuesflip <Azuesflip@gmail.com>
2014-03-06 13:46:42 -0700

    itmeNumber
    Added in an item number to separate item and left the id tag to group
    the items.

---
+++ src/InventoryInformation.java
@@ -0,0 +1,3 @@
+public class InventoryInformation {
+    public double 
+}
--- src/ProductInventory.java
+++ src/ProductInventory.java
@@ -9,15 +9,17 @@ public class ProductInventory
     public String name;
     public String description;
     public int id;
+    public int itemNumber;
     public double price;
     public int quantity;
     
     public ProductInventory(String startName, String startDescription,
-            int startId, double startPrice, int startQuantity)
+            int startId, int startItemNumber, double startPrice, int startQuantity)
     {
         name = startName;
         description = startDescription;
         id = startId;
+        itemNumber = startItemNumber;
         price = startPrice;
         quantity = startQuantity;
     }
@@ -35,6 +37,11 @@ public class ProductInventory
     public int getId()
     {
         return id;
+    }
+    
+    public int getItemNumber()
+    {
+        return itemNumber;
     }
     
     public double getPrice()
